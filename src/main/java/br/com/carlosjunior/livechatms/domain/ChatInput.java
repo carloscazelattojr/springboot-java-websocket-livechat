96916e7508c3c01581217e9ead163bfe40e27f1f
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-09-28 18:04:52 -0300

    controller created

---
+++ src/main/java/br/com/carlosjunior/livechatms/domain/ChatInput.java
@@ -0,0 +1,4 @@
+package br.com.carlosjunior.livechatms.domain;
+
+public record ChatInput(String user, String message) {
+}
---
+++ src/main/java/br/com/carlosjunior/livechatms/domain/ChatOutput.java
@@ -0,0 +1,4 @@
+package br.com.carlosjunior.livechatms.domain;
+
+public record ChatOutput(String content) {
+}
